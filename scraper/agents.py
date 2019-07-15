b194faa2bf8b7183867ee9efa9973b22bb2f0206
Joey Dreijer <joey.dreijer@ing.nl>
2019-07-15 13:59:13 +0200

    Disabled concurrent futures to do Chrome memory leak

--- scraper/agents.py
+++ scraper/agents.py
@@ -11,17 +11,21 @@ monitoring_topic = app.topic('monitoring-urls')
 
 @app.agent(matched_topic)
 async def matched_certs(matches):
-    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
-        async for match in matches:
-            url = '{0}://{1}'.format(match['proto'], match['value']).replace('*.', '')
-            executor.submit(Screenshot(url).to_png())
+    # Temp disabled multiproc to find a better solution
+    # with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+    async for match in matches:
+        url = '{0}://{1}'.format(match['proto'], match['value']).replace('*.', '')
+        Screenshot(url).to_png()
+        #executor.submit(Screenshot(url).to_png())
 
 
 @app.agent(monitoring_topic)
 async def monitor_url(urls):
-    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
-        async for url in urls:
-            executor.submit(Screenshot(url['url']).to_png())
+    # Temp disabled multiproc to find a better solution
+    # with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+    async for url in urls:
+        Screenshot(url['url']).to_png()
+        # executor.submit(Screenshot(url['url']).to_png())
 
 
 @app.timer(interval=10, on_leader=True)
